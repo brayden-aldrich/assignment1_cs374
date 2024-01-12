31191da6b65fe91ffbd0519e9fe1272b18ae0b93
Brayden Aldrich <aldricbr@oregonstate.edu>
2024-01-12 10:53:07 -0800

    Initial commit, adding assignment1.c

---
+++ assignment1.c
@@ -0,0 +1,12 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int main(void){
+    char* filePath = "./movies_sample_1.csv";
+
+    
+}
